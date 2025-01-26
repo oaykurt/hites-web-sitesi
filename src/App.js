2d8b4ac4dacf643510a6babec4ea4a7a267c488c
oaykurt <ozgeeaykurt@gmail.com>
2025-01-26 18:38:32 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+// //import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header> */}
+//       test
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function App() {
+  return (
+    <div>
+      <h1>HİTES</h1>
+      <p>Bu benim ilk denemem.</p>
+      <img src="/hites-logo-beyaz.png" alt=""/>
+      <img src={logo} alt=""/>
+    </div>
+  );
+}
+
+export default App;
